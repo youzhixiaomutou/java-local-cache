c38a6e240aa91825fe4efd8f91fd2631c943811c
Lin <linxiaowei.me@gmail.com>
2020-04-02 23:04:57 +0800

    temp

--- src/main/java/me/linxiaowei/cache/guava/GuavaTest.java
+++ src/main/java/me/linxiaowei/cache/guava/GuavaTest.java
@@ -2,11 +2,26 @@ package me.linxiaowei.cache.guava;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class GuavaTest {
 
-    public static void main(String[] args) {
-        Cache<Object, Object> cache = CacheBuilder.newBuilder().build();
+    public static void main(String[] args) throws ExecutionException {
+        Cache<Object, Object> cache = CacheBuilder.newBuilder().maximumSize(1000).expireAfterWrite(10, TimeUnit.MINUTES)
+                .build();
+        LoadingCache<String, String> lc = CacheBuilder.newBuilder().maximumSize(1000).expireAfterWrite(10, TimeUnit.MINUTES)
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String key) throws Exception {
+                        return key + ": this is value";
+                    }
+                });
+        System.out.println(lc.get("1"));
     }
 
 }
